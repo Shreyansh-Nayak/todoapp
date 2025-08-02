75778ce313a54204c7c4f78d4a30443713439bec
Shreyansh <shreyanshnayak555@gmail.com>
2025-08-02 11:57:24 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { useAuth } from './Authcontext';
 import Login from './Login'
 import Todo from "./components/Todo";
 import { Routes, Route } from 'react-router-dom';
-
+import Home from './pae/Home';
 
 function App() {
   const { user } = useAuth();
@@ -76,7 +76,9 @@ if (!user) {
   return (
     <>
       <Navbar />
-       <Routes>
+       <Routes> 
+        <Route path="/" element={<Home />} />
+
         <Route path="/todo" element={<Todo />} />
         
         <Route path="/login" element={<Login />} />
---
+++ src/pae/Home.jsx
@@ -0,0 +1,12 @@
+// Home.jsx
+import Todo from './components/Todo';
+
+const Home = () => {
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
+};
+
+export default Home;
